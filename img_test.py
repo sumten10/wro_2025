d6599e6a4d43641db72c9fe6396ac0a1877a567c
sum_ten <jssrpalma3@gmail.com>
2025-02-12 13:39:21 +0000

    si

--- img_test.py
+++ img_test.py
@@ -2,69 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 import imutils
-
-
-
-
-
-def get_centroid(contour: cv.typing.MatLike):
-    if len(contour) == 0:
-        return
-    
-    M = cv.moments(contour)
-    x = int(M["m10"] / M["m00"])
-    y = int(M["m01"] / M["m00"])
-    return (x, y)
-    
-
-
-def find_contours(image, range, ROI):
-
-    img_segmented = image[ROI[1]:ROI[3], ROI[0]:ROI[2]]
-
-    lower_mask = np.array(range[0])
-    upper_mask = np.array(range[1])
-
-    mask = cv.inRange(img_segmented, lower_mask, upper_mask)
-
-    kernel = np.ones((5,5), np.uint8)
-
-    eroded_mask = cv.erode(mask, kernel, iterations=1)
-    dilated_mask = cv.dilate(eroded_mask, kernel, iterations=1)
-
-    contours = cv.findContours(dilated_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2]
-
-    return contours
-
-
-def max_contour(contours, ROI):
-    max_area = 0
-    max_y = 0
-    max_x = 0
-    max_cnt = 0
-    
-    for c in contours:
-        
-        area = cv.contourArea(c)
-        
-        if area > 100: 
-            
-            approx=cv.approxPolyDP(c, 0.01*cv.arcLength(c,True),True)
-
-            x,y,w,h=cv.boundingRect(approx)
-
-            
-            x += ROI[0] + w // 2
-            y += ROI[1] + h
-            
-            
-            if area > max_area:
-                max_area = area
-                max_y = y
-                max_x = x
-                max_cnt = c
-
-    return [max_area, max_x, max_x, max_cnt]
+from funciones import *
 
 img = cv.imread("img/red_green.jpg")
 assert img is not None
@@ -93,7 +31,7 @@ cv.drawContours(img, green_cnts, -1, (0,255,0), 2)
 img = imutils.resize(img, 400)
 
 #cv.imshow("mask", mask)
-cv.imshow("image", img)
+cv.imshow("original", img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
---
+++ video_test.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import imutils
+from funciones import *
+
+
+
+
+cam = cv.VideoCapture(0)
+
+img_h = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+img_w = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+
+while (1):
+
+    ret, img = cam.read()
+
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+
+    mask_red = [[180-20, 120, 40], [180, 255, 255]]
+    mask_green = [[60-20, 120, 40], [60+20, 255, 255]]
+
+
+    ROI = [0, 0, img_w, img_h]
+    red_cnts = find_contours(hsv, mask_red, ROI)
+    green_cnts = find_contours(hsv, mask_green, ROI)
+
+
+
+    cv.drawContours(img, red_cnts, -1, (0,255,0), 2)
+    cv.drawContours(img, green_cnts, -1, (0,255,0), 2)
+
+
+
+
+    img = imutils.resize(img, 400)
+
+    #cv.imshow("mask", mask)
+    cv.imshow("original", img)
+    cv.waitKey(0)
+
+cv.destroyAllWindows()
+
